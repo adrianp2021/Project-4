cad5438ad50a11451e1d5dcf9a742dafa834bc73
Adrian Pantea <adrianpantea10@gmail.com>
2021-08-22 18:38:32 +0100

    test deployment 1

--- project/settings.py
+++ project/settings.py
@@ -152,10 +152,25 @@ REST_FRAMEWORK = {
     ],
 }
 
+# TESTING DJANGO MIDDLEWARE
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+
 ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' # same with this
 
 STATICFILES_DIRS = (
