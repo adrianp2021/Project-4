bb07eee444e508a33407c13220b9a662c03ba802
Adrian Pantea <adrianpantea10@gmail.com>
2021-08-22 18:02:20 +0100

    deployment

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,7 @@
 import os
 
+from pathlib import Path
+
 """
 Django settings for project project.
 
@@ -12,7 +14,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
